df1523d0b67571e43834865f0fd8a1137eaa4111
Victoria123654 <krasikova.vg@gmail.com>
2022-05-01 17:48:54 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     
+      <footer>This project is created by Viktoriia K
+     <a href="#" ></a></footer>
     </div>
   );
 }
 
-export default App;
